7c38929a627350c04a411b0f93238c85a8f90f59
Andyphied <philipandy2@gmail.com>
2021-04-28 06:44:34 +0100

    Just changed wordings

--- terraform/security-groups/variables.tf
+++ terraform/security-groups/variables.tf
@@ -1,23 +1,23 @@
 variable "server_tcp" {
-  description = "The port the server will use for TCP requests"
+  description = "The port the server used for TCP requests"
   type        = number
   default     = 22
 }
 
 variable "server_web" {
-  description = "The port the server will use for web requests"
+  description = "The port the server used for web requests"
   type        = number
   default     = 8080
 }
 
 variable "server_http" {
-  description = "The port the server will use for web requests"
+  description = "The port the server used for web requests"
   type        = number
   default     = 80
 }
 
 variable "server_https" {
-  description = "The port the server will use for web requests"
+  description = "The port the server used for web requests"
   type        = number
   default     = 443
 }
